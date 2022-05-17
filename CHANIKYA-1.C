d4c03b12ccf4f26569ba516b34eabee9140132ab
Jatin Mangla <94446159+ijatinmangla@users.noreply.github.com>
2022-05-17 05:36:15 +0530

    Add files via upload

---
+++ CHANIKYA-1.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+ int num;
+ clrscr();
+ printf("enter the integer");
+ scanf("%d" ,&num);
+ if (num % 2==0)
+ printf("%d is even",num);
+ else
+ printf("%d is odd ",num);
+ getch();
+ }
