a3f43ece959f4de41fc723febac17be52c99d46a
Anchal Singh <102603423+ianchalsingh@users.noreply.github.com>
2022-05-29 10:06:49 +0530

    Create Anchal.c
    One dimensional array

---
+++ Anchal.c
@@ -0,0 +1,23 @@
+//program to take 5 values from user and store them in array
+//Print the elements stored in the array
+
+# include <studio.h>
+int main()
+{
+int values[5];
+printf("enter 5 integers:");
+
+//Taking inputs and storing it an array
+
+for(i=0;i<5;++i){
+scanf ("%d",&values[i]);
+}
+printf ("display the integers:");
+
+//Print elements of array
+
+for(i=0;i<5;++i){
+printf("%d\n",values[i]);
+}
+return 0;
+}
