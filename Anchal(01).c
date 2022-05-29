8c9b95508bdd28eab888d30811dd61cb4c94cf79
Anchal Singh <102603423+ianchalsingh@users.noreply.github.com>
2022-05-29 13:01:08 +0530

    Anchal(01).c
    To display a number if it is negative

--- Anchal(01).c
+++ Anchal(01).c
@@ -1,9 +1,10 @@
-#include<studio.h>
+//program to display a no.if it is negative
+# include <studio.h>
 int main(){
 int number;
 printf("Enter an integer:");
-Scanf("%d",&number);
-If(number<0){
+scanf("%d",&number);
+if(number<0){
 printf("you entered %d\n",number);
 }
 printf("the if statement is easy");
