196b367d8cfbdce8ba10144fa42e0ee2e9bc6adc
Lingachandu <102347015+Lingachandu@users.noreply.github.com>
2022-05-30 19:14:09 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+void main()
+{
+    char a[50];
+    int l=0;
+    printf("enter the string :");
+    gets(a);
+    for( int i=0;a[i]!= \'0';i++);
+    l++;
+    printf("lenth of the string is :%d",l);
+    
+}
